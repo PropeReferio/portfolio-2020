dba0a696f8f90562a9386d6ceb0def76952cbc7d
PropeReferio <lemuel.b.stevens@gmail.com>
2020-10-13 10:26:18 -0500

    fonts, hover social, hover navbar

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Social() {
+  const [isLinkedinBlack, setIsLinkedinBlack] = useState(true);
+  const [isGithubBlack, setIsGithubBlack] = useState(true);
+
   return (
     <div className="socialIcons">
-      <a href="https://github.com/PropeReferio">
-        <FaGithub size={44} color="black" />
+      <a onMouseEnter={() => setIsGithubBlack(false)}
+        onMouseLeave={() => setIsGithubBlack(true)}
+        href="https://github.com/PropeReferio">
+        <FaGithub size={44} style={{ fill: isGithubBlack ? '#fff' : '#262626' }} />
       </a>
-      <a href="https://www.linkedin.com/in/bostevens-a-softwareengineer/">
-        <FaLinkedin size={44} color="black" />
-        </a>
+      <a onMouseEnter={() => setIsLinkedinBlack(false)}
+        onMouseLeave={() => setIsLinkedinBlack(true)}
+        href="https://www.linkedin.com/in/bostevens-a-softwareengineer/">
+        <FaLinkedin size={44} style={{ fill: isLinkedinBlack ? '#fff' : '#262626' }} />
+      </a>
     </div>
   )
 }
