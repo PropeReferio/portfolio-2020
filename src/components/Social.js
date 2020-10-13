26770890b5462efeab3f9d8c8406f7ba2ed22687
PropeReferio <lemuel.b.stevens@gmail.com>
2020-10-13 10:29:15 -0500

    added phone to social

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaPhone } from "react-icons/fa";
 
 export default function Social() {
   const [isLinkedinBlack, setIsLinkedinBlack] = useState(true);
   const [isGithubBlack, setIsGithubBlack] = useState(true);
+  const [isPhoneBlack, setIsPhoneBlack] = useState(true);
 
   return (
     <div className="socialIcons">
@@ -16,6 +17,11 @@ export default function Social() {
         onMouseLeave={() => setIsLinkedinBlack(true)}
         href="https://www.linkedin.com/in/bostevens-a-softwareengineer/">
         <FaLinkedin size={44} style={{ fill: isLinkedinBlack ? '#fff' : '#262626' }} />
+      </a>
+      <a onMouseEnter={() => setIsPhoneBlack(false)}
+        onMouseLeave={() => setIsPhoneBlack(true)}
+        href="tel: +1 615-788-9366">
+        <FaPhone size={36} style={{ fill: isPhoneBlack ? '#fff' : '#262626' }} />
       </a>
     </div>
   )
