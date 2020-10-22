5c4334da1c97487ee98d0a6d11f9b43487d872f9
PropeReferio <lemuel.b.stevens@gmail.com>
2020-10-22 13:57:56 -0500

    having problems with map

--- src/data/sliderData.js
+++ src/data/sliderData.js
@@ -1,7 +1,6 @@
 export const images = [
     'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
     'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
-    'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
     'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
   ]
 export const testimonials = [
@@ -17,34 +16,24 @@ export const testimonials = [
     I brought Bo on for a data visualization project, which he 
     quickly brought to fruition with minimal guidance. He 
     selected the technologies and architecture for the project,
-     learning new methodologies as necessary. I can personally 
-     tell how passionate he is about this job, by how quickly 
-     he learns new tools, and how excited he is to implement 
-     them. He is also a great communicator and explains his 
-     work very clearly. Bo would be an asset to any company.`,
-     `Working with a self-starter like Bo has been so refreshing.
-     I brought Bo on for a data visualization project, which he 
-     quickly brought to fruition with minimal guidance. He 
-     selected the technologies and architecture for the project,
-      learning new methodologies as necessary. I can personally 
-      tell how passionate he is about this job, by how quickly 
-      he learns new tools, and how excited he is to implement 
-      them. He is also a great communicator and explains his 
-      work very clearly. Bo would be an asset to any company.`,
-      `Working with a self-starter like Bo has been so refreshing.
-      I brought Bo on for a data visualization project, which he 
-      quickly brought to fruition with minimal guidance. He 
-      selected the technologies and architecture for the project,
-       learning new methodologies as necessary. I can personally 
-       tell how passionate he is about this job, by how quickly 
-       he learns new tools, and how excited he is to implement 
-       them. He is also a great communicator and explains his 
-       work very clearly. Bo would be an asset to any company.`
+    learning new methodologies as necessary. I can personally 
+    tell how passionate he is about this job, by how quickly 
+    he learns new tools, and how excited he is to implement 
+    them. He is also a great communicator and explains his 
+    work very clearly. Bo would be an asset to any company.`,
+    `Working with a self-starter like Bo has been so refreshing.
+    I brought Bo on for a data visualization project, which he 
+    quickly brought to fruition with minimal guidance. He 
+    selected the technologies and architecture for the project,
+    learning new methodologies as necessary. I can personally 
+    tell how passionate he is about this job, by how quickly 
+    he learns new tools, and how excited he is to implement 
+    them. He is also a great communicator and explains his 
+    work very clearly. Bo would be an asset to any company.`,
   ]
   
 export const authors = [
     `Andy Malkin, Senior Fullstack Engineer, Chainstarters`,
-    `Arjun Srivastava, Software Engineer, Cloud Cafe Tech`,
     `Arjun Srivastava, Software Engineer, Cloud Cafe Tech`,
     `Arjun Srivastava, Software Engineer, Cloud Cafe Tech`,
   ]
