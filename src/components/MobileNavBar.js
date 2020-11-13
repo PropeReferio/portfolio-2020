80c7aefef0c04af56c16438d6c17646017c8081b
PropeReferio <lemuel.b.stevens@gmail.com>
2020-11-13 12:03:35 -0500

    functional, but needs styling

--- src/components/MobileNavBar.js
+++ src/components/MobileNavBar.js
@@ -1,12 +1,60 @@
-import React from 'react';
-import { FaHamburger, FaHSquare, FaMobile } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaHamburger } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 export default function MobileNavBar() {
+	const [isVisible, setIsVisible] = useState(false)
+
+	function toggleIsVisible () {
+		setIsVisible(!isVisible);
+	}
 	return(
 		<div id="mobileNavBar">
-			<FaHamburger size={38} style={{ fill: '#fff' }} />
-			<FaHSquare size={38} style={{ fill: '#fff' }} />
-			<FaMobile size={38} style={{ fill: '#fff' }} />
-		</div>
+			<div id="mobileNavButton">
+				<FaHamburger 
+					size={38} 
+					onClick={toggleIsVisible}
+					style={{ fill: '#fff' }} />
+			</div>
+			<div id="mobileNavLinks"
+			style={{ display: isVisible ? "block" : "none" }}
+			>
+				<nav>
+					<ul>
+						<li id='Home'>
+							<Link to='/'>
+								Home
+							</Link>
+						</li>
+						<li id='About'>
+							<Link to='/about'>
+								About
+							</Link>
+						</li>
+						<li id='Projects'>
+							<Link to='/projects'>
+								Projects
+							</Link>
+						</li>
+						<li id='Skills'>
+							<a href="/#skills">
+								Skills
+							</a>
+						</li>
+						<li id='Referrals'>
+							<a href="/#referrals"> 
+								Referrals
+							</a>
+						</li>
+						<li id='Contact'>
+							<Link to='/contact'>
+								Contact
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+	</div>
+		
 	)
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav id="desktopNav">
       <ul>
         <li id='Home'>
           <Link to='/'>
