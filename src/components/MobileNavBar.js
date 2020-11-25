79c11f7bf3beba9363fc77a431a05c880d28f06b
PropeReferio <lemuel.b.stevens@gmail.com>
2020-11-25 11:37:22 -0600

    Profile photo responsive

--- src/components/MobileNavBar.js
+++ src/components/MobileNavBar.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { FaHamburger, FaBars } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaBars } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 export default function MobileNavBar() {
